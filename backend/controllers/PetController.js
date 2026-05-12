14b1b808f1501803fb4f341dd1071b3502705ecb
PedroPenitenti <pedrohppenitenti@gmail.com>
2026-05-12 20:23:21 -0300

    Task 1: Estruturação das Rotas (PetRoutes.js)

---
+++ backend/controllers/PetController.js
@@ -0,0 +1,43 @@
+const Pet = require('../models/Pet')
+const User = require('../models/User')
+const getToken = require('../helpers/get-tokens')
+const getUserByToken = require('../helpers/get-user-by-token')
+const mongoose = require('mongoose')
+
+module.exports = class PetController {
+    static async create(req, res) {
+       
+    }
+
+    static async getAllPets(req, res) {
+        
+    }
+
+    static async getAllUserPets(req, res) {
+        
+    }
+
+    static async getAllUserAdoptions(req, res) {
+        
+    }
+
+    static async getPetById(req, res) {
+        
+    }
+
+    static async updatePet(req, res) {
+        
+    }
+
+    static async removePetById(req, res) {
+        
+    }
+
+    static async agendamento(req, res) {
+        
+    }
+
+    static async concluirAdoption(req, res) {
+        
+    }
+}
---
+++ backend/routers/PetRouters.js
@@ -0,0 +1,42 @@
+const router = require('express').Router()
+
+const PetController = require('../controllers/PetController')
+
+const verifyToken = require('../helpers/verify-token')
+const { imageUpload } = require('../helpers/image-upload')
+
+router.post('/create',
+    verifyToken,
+    imageUpload.single('image'),
+    PetController.create)
+
+router.get('/mypets',
+    verifyToken,
+    PetController.getAllUserPets)
+
+router.get('/myadoptions',
+    verifyToken,
+    PetController.getAllUserAdoptions)
+
+router.get('/', PetController.getAllPets)
+
+router.get('/:id', PetController.getPetById)
+
+router.patch('/edit/:id',
+    verifyToken,
+    imageUpload.single('image'),
+    PetController.updatePet)
+
+router.delete('/delete/:id',
+    verifyToken,
+    PetController.removePetById)
+
+router.post('/:id/schedule',
+    verifyToken,
+    PetController.agendamento)
+
+router.patch('/:id/conclude',
+    verifyToken,
+    PetController.concluirAdoption)
+
+module.exports = router
