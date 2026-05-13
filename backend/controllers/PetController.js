299de55d1d951275ee40443f52304225c503322c
PedroPenitenti <pedrohppenitenti@gmail.com>
2026-05-12 23:02:51 -0300

    Task 1: Estruturação das Rotas (PetRoutes.js)

---
+++ backend/controllers/PetController.js
@@ -0,0 +1,42 @@
+const Pet = require('../models/Pet')
+const User = require('../models/User')
+const getToken = require('../helpers/get-tokens')
+const getUserByToken = require('../helpers/get-user-by-token')
+const mongoose = require('mongoose')
+
+module.exports = class PetController {
+    static async create(req, res) {
+       
+    }
+
+    static async getAllPets(req, res) {
+
+    }
+
+    static async getAllUserPets(req, res) {
+       
+    }
+
+    static async getAllUserAdoptions(req, res) {
+
+    }
+
+    static async getPetById(req, res) {
+
+    }
+
+    static async updatePet(req, res) {
+        
+    }
+
+    static async removePetById(req, res) {
+       
+    }
+
+    static async schedule(req, res) {
+        
+    }
+
+    static async concludeAdoption(req, res) {
+    }
+}
